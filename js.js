c04810c853e78b7caac85e4914f0d0a64d9d820e
Meldia <diaz.melinajimena@gmail.com>
2022-04-20 23:55:15 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+//SIDEBAR
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.add('active')
+    })
+})
